9aa0a3da32c8359daade9a12c13aedc5caf46909
kazemek <>
2013-07-12 17:38:56 +0200

    PM-412 restructure plugin folders and also add payment class

---
+++ xt_paymill/classes/payment/Paymill.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(dirname(dirname(__FILE__)) . '/lib/Services/Paymill/PaymentProcessor.php');
+require_once(dirname(dirname(__FILE__)) . '/lib/Services/Paymill/LoggingInterface.php');
+
+class Paymill implements Services_Paymill_LoggingInterface
+{
+    /**
+     * @var \Services_Paymill_PaymentProcessor
+     */
+    private $_paymentProcessor;
+    
+    private $_bridgeUrl = 'https://bridge.paymill.com/';
+    
+    private $_apiUrl    = 'https://api.paymill.com/v2/';
+    
+    public function __construct()
+    {
+        $this->_paymentProcessor = new Services_Paymill_PaymentProcessor();
+    }
+    
+    public function log($message, $debugInfo)
+    {
+        $logfile = _SRV_WEBROOT . '/plugins/xt_paymill/log/log.txt';
+        if (file_exists($logfile) && is_writable($logfile)) {
+            $handle = fopen($logfile, 'a+');
+            fwrite($handle, "[" . date(DATE_RFC822) . "] " . $message . "\n");
+            fwrite($handle, "[" . date(DATE_RFC822) . "] " . $debugInfo . "\n");
+            fclose($handle);
+        }
+    }
+}
