5ad96a64f52a4f738ec7933906e3adf37e0aa441
kazemek <>
2013-07-25 13:36:57 +0200

    PM-465 now we update the transaction description to the right order id

--- xt_paymill/classes/class.xt_paymill.php
+++ xt_paymill/classes/class.xt_paymill.php
@@ -6,6 +6,7 @@ require_once(dirname(__FILE__) . '/lib/Services/Paymill/PaymentProcessor.php');
 require_once(dirname(__FILE__) . '/lib/Services/Paymill/LoggingInterface.php');
 require_once(dirname(__FILE__) . '/lib/Services/Paymill/Clients.php');
 require_once(dirname(__FILE__) . '/lib/Services/Paymill/Payments.php');
+require_once(dirname(__FILE__) . '/lib/Services/Paymill/Transactions.php');
 require_once(dirname(__FILE__) . '/helpers/FastCheckout.php');
 
 class xt_paymill implements Services_Paymill_LoggingInterface
@@ -16,18 +17,18 @@ class xt_paymill implements Services_Paymill_LoggingInterface
      * @var string
      */
     public $version = '2.1.0';
-    
+
     /**
      * @var boolean
      */
     public $subpayments = true;
-    
+
     /**
      * Allowed subpayments (cc, dd)
      * @var array
      */
     public $allowed_subpayments;
-    
+
     /**
      * Template data
      * @var array
@@ -38,17 +39,22 @@ class xt_paymill implements Services_Paymill_LoggingInterface
      * @var \Services_Paymill_PaymentProcessor
      */
     private $_paymentProcessor;
-    
+
     /**
      * @var \Services_Paymill_Clients 
      */
     private $_clients;
-    
+
     /**
      * @var \Services_Paymill_Clients 
      */
     private $_payments;
-    
+
+    /**
+     * @var \Services_Paymill_Transactions 
+     */
+    private $_transactions;
+
     /**
      * Api endpoint
      * @var string
@@ -57,50 +63,60 @@ class xt_paymill implements Services_Paymill_LoggingInterface
 
     public function __construct()
     {
+        global $page;
+        
         $this->_fastCheckout = new FastCheckout();
-        $this->_payments = new Services_Paymill_Payments(
-            trim($this->_getPaymentConfig('PRIVATE_API_KEY')), 
+        
+        $this->_transactions = new Services_Paymill_Transactions(
+            trim($this->_getPaymentConfig('PRIVATE_API_KEY')),
             $this->_apiUrl
         );
         
-        $this->_clients = new Services_Paymill_Clients(
-            trim($this->_getPaymentConfig('PRIVATE_API_KEY')), 
-            $this->_apiUrl   
+        $this->_payments = new Services_Paymill_Payments(
+            trim($this->_getPaymentConfig('PRIVATE_API_KEY')),
+            $this->_apiUrl
         );
-        
+
+        $this->_clients = new Services_Paymill_Clients(
+            trim($this->_getPaymentConfig('PRIVATE_API_KEY')),
+            $this->_apiUrl
+        );
+
         $this->_setCheckoutData();
-        
+
         $this->_paymentProcessor = new Services_Paymill_PaymentProcessor();
         $this->_paymentProcessor->setApiUrl($this->_apiUrl);
         $this->_paymentProcessor->setLogger($this);
         $this->_paymentProcessor->setPrivateKey(trim($this->_getPaymentConfig('PRIVATE_API_KEY')));
         $this->_paymentProcessor->setSource($this->version . '_xt:Commerce_' . _SYSTEM_VERSION);
-        $this->allowed_subpayments = array('cc', 'dd');    
+        $this->allowed_subpayments = array('cc', 'dd');
+        
+        if ($page->page_name == 'checkout' && $page->page_action == 'success') {
+            $this->_success();
+        }
     }
-    
+
     private function _setCheckoutData()
     {
         global $currency, $page;
-        
+
         if ($page->page_name == 'checkout' && $page->page_action == 'payment') {
             unset($_SESSION['paymillAuthorizedAmount']);
             $_SESSION['paymillAuthorizedAmount'] = (int) round(
-                ($_SESSION['cart']->total_physical['plain'] + $this->_getPaymentConfig('DIFFERENT_AMOUNT')) * 100
+                            ($_SESSION['cart']->total_physical['plain'] + $this->_getPaymentConfig('DIFFERENT_AMOUNT')) * 100
             );
         }
-        
+
         $this->data['xt_paymill']['fast_checkout_cc'] = $this->_fastCheckout->canCustomerFastCheckoutCcTemplate(
-            $_SESSION["customer"]->customers_id
+                $_SESSION["customer"]->customers_id
         );
-        
+
         $this->data['xt_paymill']['fast_checkout_elv'] = $this->_fastCheckout->canCustomerFastCheckoutElvTemplate(
-            $_SESSION["customer"]->customers_id
+                $_SESSION["customer"]->customers_id
         );
-        
+
         $data = $this->_fastCheckout->loadFastCheckoutData($_SESSION['customer']->customers_id);
-        
-        
-        
+
         if (!empty($data->paymentID_CC)) {
             $payment = $this->_payments->getOne($data->paymentID_CC);
             $this->data['xt_paymill']['cc_number'] = '************' . $payment['last4'];
@@ -108,22 +124,22 @@ class xt_paymill implements Services_Paymill_LoggingInterface
             $this->data['xt_paymill']['cvc'] = '***';
             $this->data['xt_paymill']['card_holder'] = $payment['card_holder'];
         }
-        
+
         if (!empty($data->paymentID_ELV)) {
             $payment = $this->_payments->getOne($data->paymentID_ELV);
             $this->data['xt_paymill']['bank_code'] = $payment['code'];
             $this->data['xt_paymill']['account_holder'] = $payment['holder'];
             $this->data['xt_paymill']['account_number'] = $payment['account'];
         }
-        
+
         $this->data['xt_paymill']['currency'] = $currency->code;
         $this->data['xt_paymill']['amount'] = $_SESSION['paymillAuthorizedAmount'];
-        
+
         if (array_key_exists('xt_paymill_cc_error', $_SESSION)) {
             $this->data['xt_paymill']['error_cc'] = $_SESSION['xt_paymill_cc_error'];
             unset($_SESSION['xt_paymill_cc_error']);
         }
-        
+
         if (array_key_exists('xt_paymill_dd_error', $_SESSION)) {
             $this->data['xt_paymill']['error_elv'] = $_SESSION['xt_paymill_dd_error'];
             unset($_SESSION['xt_paymill_dd_error']);
@@ -139,88 +155,88 @@ class xt_paymill implements Services_Paymill_LoggingInterface
             $_SESSION[$code . '_error'] = TEXT_PAYMILL_ERR_TOKEN;
             $xtLink->_redirect($xtLink->_link(array('page' => 'checkout', 'paction' => 'payment', 'conn' => 'SSL')));
         } else {
-            
+
             $this->_setTransaction($code);
-            
+
             $data = $this->_fastCheckout->loadFastCheckoutData($_SESSION['customer']->customers_id);
             if (!empty($data->clientID)) {
                 $this->_existingClient($data);
             }
-            
+
             if ($token === 'dummyToken') {
                 $this->_fastCheckout($code);
             }
-            
+
             $this->_paymentProcessor->setToken($token);
             unset($_SESSION['token']);
-            
+
             if (!$this->_paymentProcessor->processPayment()) {
                 $_SESSION[$code . '_error'] = TEXT_PAYMILL_ERR_ORDER;
                 $xtLink->_redirect($xtLink->_link(array('page' => 'checkout', 'paction' => 'payment', 'conn' => 'SSL')));
             }
-            
+
             if ($this->_getPaymentConfig('FAST_CHECKOUT') === 'true') {
                 $this->_savePayment($code);
             }
+
+            unset($_SESSION['paymillAuthorizedAmount']);
+
+            $_SESSION['paymillTransactionId'] = $this->_paymentProcessor->getTransactionId();
         }
     }
-    
+
     private function _savePayment($code)
     {
         if ($code === 'xt_paymill_cc') {
             $this->_fastCheckout->saveCcIds(
-                $_SESSION['customer']->customers_id, 
-                $this->_paymentProcessor->getClientId(), 
-                $this->_paymentProcessor->getPaymentId()
+                $_SESSION['customer']->customers_id, $this->_paymentProcessor->getClientId(), $this->_paymentProcessor->getPaymentId()
             );
         }
 
         if ($code === 'xt_paymill_dd') {
             $this->_fastCheckout->saveElvIds(
-                $_SESSION['customer']->customers_id, 
-                $this->_paymentProcessor->getClientId(), 
-                $this->_paymentProcessor->getPaymentId()
+                $_SESSION['customer']->customers_id, $this->_paymentProcessor->getClientId(), $this->_paymentProcessor->getPaymentId()
             );
         }
     }
-    
+
     private function _setTransaction($code)
     {
         global $currency;
-        
+
         $name = $_SESSION['customer']->customer_payment_address['customers_firstname']
-              . ' '
-              . $_SESSION['customer']->customer_payment_address['customers_lastname'];
+                . ' '
+                . $_SESSION['customer']->customer_payment_address['customers_lastname'];
 
         $this->_paymentProcessor->setAmount((int) round($_SESSION['cart']->total_physical['plain'] * 100));
 
         $this->_paymentProcessor->setEmail($_SESSION['customer']->customer_info['customers_email_address']);
         $this->_paymentProcessor->setName($name);
         $this->_paymentProcessor->setCurrency($currency->code);
-        $this->_paymentProcessor->setDescription(_STORE_NAME . ' Order ID: ' . $this->_getNextOrderId());
-        
+        $this->_paymentProcessor->setDescription(_STORE_NAME);
+
         if ($code === 'xt_paymill_cc') {
             $this->_paymentProcessor->setPreAuthAmount($_SESSION['paymillAuthorizedAmount']);
             unset($_SESSION['paymillAuthorizedAmount']);
         }
     }
-    
+
     private function _existingClient($data)
     {
         $client = $this->_clients->getOne($data->clientID);
         if ($client['email'] !== $_SESSION['customer']->customer_info['customers_email_address']) {
             $this->_clients->update(
-                array(
-                    'id' => $data->clientID,
-                    'email' => $_SESSION['customer']->customer_info['customers_email_address']
-                )
+                    array(
+                        'id' => $data->clientID,
+                        'email' => $_SESSION['customer']->customer_info['customers_email_address']
+                    )
             );
         }
 
         $this->_paymentProcessor->setClientId($client['id']);
     }
-    
-    private function _fastCheckout($code) 
+
+    private function _fastCheckout($code)
     {
         if ($this->_fastCheckout->canCustomerFastCheckoutCc($_SESSION['customer']->customers_id) && $code === 'xt_paymill_cc') {
             $data = $this->_fastCheckout->loadFastCheckoutData($_SESSION['customer']->customers_id);
@@ -237,11 +253,6 @@ class xt_paymill implements Services_Paymill_LoggingInterface
         }
     }
 
-    private function _getNextOrderId()
-    {
-        return $_SESSION['last_order_id'] + 1;
-    }
-
     public function log($message, $debugInfo)
     {
         if ($this->_getPaymentConfig('DEBUG_MODE') === 'true') {
@@ -254,7 +265,7 @@ class xt_paymill implements Services_Paymill_LoggingInterface
             }
         }
     }
-    
+
     private function _isTokenAvailable($data)
     {
         return !empty($data);
@@ -268,6 +279,19 @@ class xt_paymill implements Services_Paymill_LoggingInterface
         }
 
         return $value;
+    }
+
+    private function _success()
+    {
+        $this->_transactions->update(
+            array(
+                'id' => $_SESSION['paymillTransactionId'],
+                'description' => _STORE_NAME . ' Order ID: ' . $_SESSION['success_order_id']
+            )
+        );
+
+
+        unset($_SESSION['paymillTransactionId']);
     }
 
 }
