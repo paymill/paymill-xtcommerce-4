a320b3a112fb5e0094ac94f44eea97df4deedfde
kazemek <>
2014-01-08 15:18:53 +0100

    PMVE-7 add detectCreditcardBranding

--- xt_paymill/js/cc.js
+++ xt_paymill/js/cc.js
@@ -1,12 +1,12 @@
 $(document).ready(function()
 {
 	var submitFlag = false;
-	
+
 	var cssClass = "paymill-card-number-";
 
-	$('#paymill-card-number').keyup(function() {
-
-		switch (paymill.cardType($('#paymill-card-number').val()).toLowerCase()) {
+	$('#paymill-card-number').keyup(function() 
+	{
+		switch (detectCreditcardBranding($('#paymill-card-number').val()).toLowerCase()) {
 			case 'visa':
 				$('#paymill-card-number').removeClass();
 				$('#paymill-card-number').addClass('paymill-input ' + cssClass + 'visa');
@@ -40,7 +40,50 @@ $(document).ready(function()
 				$('#paymill-card-number').addClass('paymill-input ' + cssClass + 'unionpay');
 				break;
 		}
-	});	
+	});
+
+	function detectCreditcardBranding(creditcardNumber) 
+	{
+		var brand = 'unknown';
+		if (creditcardNumber.match(/^\d{6}/)) {
+			switch (true) {
+				case /^(415006|497|407497|513)/.test(creditcardNumber):
+					brand = "carte bleue";
+					break;
+				case /^(45399[78]|432913|5255)/.test(creditcardNumber):
+					brand = "carta si";
+					break;
+				case /^(4571|5019)/.test(creditcardNumber):
+					brand = "dankort";
+					break;
+				case /^(62|88)/.test(creditcardNumber):
+					brand = "china unionpay";
+					break;
+				case /^6(011|5)/.test(creditcardNumber):
+					brand = "discover";
+					break;
+				case /^3(0[0-5]|[68])/.test(creditcardNumber):
+					brand = "diners club";
+					break;
+				case /^(5018|5020|5038|5893|6304|6759|6761|6762|6763|0604|6390)/.test(creditcardNumber):
+					brand = "maestro";
+					break;
+				case /^(2131|1800|35)/.test(creditcardNumber):
+					brand = "jcb";
+					break;
+				case /^(3[47])/.test(creditcardNumber):
+					brand = "amex";
+					break;
+				case /^(5[1-5])/.test(creditcardNumber):
+					brand = "mastercard";
+					break;
+				case /^(4)/.test(creditcardNumber):
+					brand = "visa";
+					break;
+			}
+		}
+		return brand;
+	}
 
 	function paymillCcResponseHandler(error, result)
 	{
@@ -82,7 +125,7 @@ $(document).ready(function()
 			ccErrorFlag = false;
 		}
 
-		if (!paymill.validateCvc($('#paymill-card-cvc').val()) && paymill.cardType($('#paymill-card-number').val()).toLowerCase() !== 'maestro') {
+		if (!paymill.validateCvc($('#paymill-card-cvc').val()) && detectCreditcardBranding($('#paymill-card-number').val()).toLowerCase() !== 'maestro') {
 			$("#payment-error-cc-2").text(lang['verfication_number_invalid']);
 			$("#payment-error-cc-2").css('display', 'block');
 			ccErrorFlag = false;
@@ -139,7 +182,7 @@ $(document).ready(function()
 		fastCheckoutCc = 'false';
 		$('#paymill-card-holdername').val('');
 	});
-	
+
 	$('form[name^="process"]').submit(function(event) {
 		if (!submitFlag) {
 			event.preventDefault();
